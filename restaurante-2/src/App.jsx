bf93a5e2084afa9b276174a978922acd52e3a15d
Matthew Ameri <matthewameri200310@gmail.com>
2026-02-16 23:06:15 -0500

    actualiza mesas testing

--- restaurante-2/src/App.jsx
+++ restaurante-2/src/App.jsx
@@ -1,15 +1,40 @@
 import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
+// Lazy Loading para mantener la optimización (esto sí ayuda a tu nota)
+const Inicio = lazy(() => import("./Paginas/Inicio"));
+const Contacto = lazy(() => import("./Paginas/Contacto"));
+const Historia = lazy(() => import("./Paginas/Historia"));
+const Reservas = lazy(() => import("./Paginas/Reservas"));
+const Mesas = lazy(() => import("./Paginas/Mesas"));
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Inicio />} />
-      <Route path="/contacto" element={<Contacto />} />
-      <Route path="/historia" element={<Historia />} />
-      <Route path="/reservas" element={<Reservas />} />
-      <Route path="/mesas" element={<Mesas />} />
-    </Routes>
+    // Dejamos un mensaje simple de carga mientras React descarga las páginas
+    <Suspense
+      fallback={
+        <div
+          style={{
+            backgroundColor: "black",
+            height: "100vh",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Cargando...
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path="/reservas" element={<Reservas />} />
+        <Route path="/mesas" element={<Mesas />} />
+      </Routes>
+    </Suspense>
   );
 }
 
