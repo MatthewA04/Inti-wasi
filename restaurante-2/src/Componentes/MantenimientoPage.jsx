f70fa7a8f27c527d694d9f013a29a16735337d92
Matthew Ameri <matthewameri200310@gmail.com>
2026-02-18 22:14:22 -0500

    Anadir mantenimiento

--- restaurante-2/src/Componentes/MantenimientoPage.jsx
+++ restaurante-2/src/Componentes/MantenimientoPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./MaintenancePage.css";
 
-const MaintenancePage = () => {
+const MantenimientoPage = () => {
   const navigate = useNavigate();
 
   return (
