79c150552cc33a5797d30375b67cf621d8486f98
Matthew Ameri <matthewameri200310@gmail.com>
2026-02-19 00:17:52 -0500

    Anadir mantenimiento

--- restaurante-2/src/Step5Datos.test.jsx
+++ restaurante-2/src/Step5Datos.test.jsx
@@ -1,21 +1,16 @@
-// src/Step5Datos.test.jsx
-
 import React from "react";
 import { jest } from "@jest/globals";
 
-// ESTE ES EL TRUCO: Mockeamos el archivo env.js para que Jest no lo lea
 jest.mock("./env.js", () => ({
   VITE_API_TOKEN: "token-de-prueba-seguro",
 }));
 
-// Ahora sí, las importaciones normales
 import { render, screen } from "@testing-library/react";
 import { ReservaProvider } from "./Data/ReservaContext.jsx";
 import Step5Datos from "./Componentes/Step5Datos.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 
-// ... resto de tu código de test
 test("El botón de continuar debe estar deshabilitado si el formulario está vacío", () => {
   render(
     <ReservaProvider>
@@ -26,5 +21,5 @@ test("El botón de continuar debe estar deshabilitado si el formulario está vac
   );
 
   const boton = screen.getByRole("button", { name: /continuar/i });
-  expect(boton).toBeDisabled(); // Verifica la lógica de validación que pusimos
+  expect(boton).toBeDisabled();
 });
